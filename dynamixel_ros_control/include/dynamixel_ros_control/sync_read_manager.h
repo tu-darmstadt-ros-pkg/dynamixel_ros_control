7b00ae8e919948931eb7950d30716566889fa496
Martin Oehler <oehler@sim.tu-darmstadt.de>
2019-07-03 20:58:43 +1000

    added missing pointer initialization

--- dynamixel_ros_control/include/dynamixel_ros_control/sync_read_manager.h
+++ dynamixel_ros_control/include/dynamixel_ros_control/sync_read_manager.h
@@ -8,9 +8,9 @@
 namespace dynamixel_ros_control {
 
 struct DxlValue {
-  DxlValue(double* _dvalue)  : dvalue(_dvalue) {}
-  DxlValue(bool* _bvalue)    : bvalue(_bvalue) {}
-  DxlValue(int32_t* _ivalue) : ivalue(_ivalue) {}
+  DxlValue(double* _dvalue)  : dvalue(_dvalue), bvalue(nullptr), ivalue(nullptr) {}
+  DxlValue(bool* _bvalue)    : dvalue(nullptr), bvalue(_bvalue), ivalue(nullptr) {}
+  DxlValue(int32_t* _ivalue) : dvalue(nullptr), bvalue(nullptr), ivalue(_ivalue) {}
   double *dvalue;
   bool *bvalue;
   int32_t *ivalue;
