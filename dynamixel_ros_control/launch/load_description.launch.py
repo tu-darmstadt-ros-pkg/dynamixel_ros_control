2f04a90c2ad911d83b6702bcb378a4c61c2b0b43
Martin Oehler <oehler@sim.tu-darmstadt.de>
2025-03-06 18:16:27 +0100

    Added parameters for port name, baud rate and dynamixel id

--- dynamixel_ros_control/launch/load_description.launch.py
+++ dynamixel_ros_control/launch/load_description.launch.py
@@ -8,6 +8,31 @@ from launch_ros.parameter_descriptions import ParameterValue
 def generate_launch_description():
     ld = LaunchDescription()
 
+    # Parameters
+    port_name_arg = DeclareLaunchArgument(
+        name='port_name',
+        default_value='/dev/ttyUSB0',
+        description='Path to USB serial converter device'
+    )
+    ld.add_action(port_name_arg)
+    port_name = LaunchConfiguration('port_name')
+
+    baud_rate_arg = DeclareLaunchArgument(
+        name='baud_rate',
+        default_value='57600',
+        description='Baud rate'
+    )
+    ld.add_action(baud_rate_arg)
+    baud_rate = LaunchConfiguration('baud_rate')
+
+    id_arg = DeclareLaunchArgument(
+        name='id',
+        default_value='1',
+        description='ID of the dynamixel to control'
+    )
+    ld.add_action(id_arg)
+    id_arg = LaunchConfiguration('id')
+
     # Get the package directory
     urdf_file = PathJoinSubstitution([
         get_package_share_directory('dynamixel_ros_control'), 'config', 'test_robot.urdf.xacro'
@@ -15,7 +40,7 @@ def generate_launch_description():
 
     robot_description = ParameterValue(
         Command([
-            'xacro ', urdf_file,
+            'xacro ', urdf_file, ' port_name:=', port_name, ' baud_rate:=', baud_rate, ' id:=', id_arg,
                  ]),
                  value_type=str
     )
